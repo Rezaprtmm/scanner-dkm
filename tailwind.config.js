94d51e5d789ea754857d0fc4fcfe9f1c48b02f07
Rezaprtmm <rizki.pratama@students.paramadina.ac.id>
2023-05-21 17:10:24 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,64 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#FBFDFF",
+          200: "#F8FBFF",
+          300: "#F0F6FF",
+          400: "#E9F2FF",
+          500: "#E1EDFF",
+          600: "#DAE9FF",
+          700: "#AEBACC",
+          800: "#838C99",
+          900: "#575D66",
+          1000: "#2C2F33",
+        },
+        ribbon: {
+          100: "#E6F0FF",
+          200: "#CCE0FF",
+          300: "#99C2FF",
+          400: "#66A3FF",
+          500: "#3385FF",
+          600: "#0066FF",
+          700: "#0052CC",
+          800: "#003D99",
+          900: "#002966",
+          1000: "#001433",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "608px",
+          },
+          "@screen md": {
+            maxWidth: "736px",
+          },
+          "@screen lg": {
+            maxWidth: "992px",
+          },
+          "@screen xl": {
+            maxWidth: "1180px",
+          },
+        },
+      })
+    },
+    require("@tailwindcss/line-clamp"),
+  ],
 }
